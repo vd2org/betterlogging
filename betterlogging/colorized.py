c689b04e6b9aa9a65c90613344dca0c161caa3e6
vd <vd@>
2022-02-16 22:03:38 +0200

    Updated copyright.

--- betterlogging/colorized.py
+++ betterlogging/colorized.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 by Vd.
+# Copyright (C) 2020-2022 by Vd.
 # This file is part of BetterLogging, my logging improvement.
 # BetterLogging is released under the MIT License (see LICENSE).
 
