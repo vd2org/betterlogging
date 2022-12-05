38a088e0f212e1013caa1093046a447ad2c57699
benyaming <benyaming@appcard.com>
2022-12-05 21:15:15 +0200

    Support `NO_COLOR` env

--- betterlogging/colorized.py
+++ betterlogging/colorized.py
@@ -4,6 +4,7 @@
 
 
 import logging
+import os
 import sys
 from logging import Formatter
 from logging import LogRecord
@@ -12,9 +13,20 @@ from .colors import colors
 from .outer.better_exceptions import ExceptionFormatter
 from .trace import TRACE
 
+
+# support for https://no-color.org/
+if os.environ.get('NO_COLOR'):
+    NO_COLOR = True
+else:
+    NO_COLOR = False
+
 DEFAULT_FORMAT = '%(c_fg_green)s%(asctime)s %(c_color)s%(levelname)-8s%(c_reset)s %(c_fg_cyan)s[%(name)s] ' \
                  '%(c_reset)s(%(filename)s:%(c_underscore)s%(funcName)s%(c_reset)s:%(lineno)d) ' \
                  '%(c_color)s%(message)s%(c_reset)s'
+
+if NO_COLOR:
+    DEFAULT_FORMAT = '%(asctime)s %(levelname)-8s [%(name)s] ' \
+                     '(%(filename)s:%(funcName)s:%(lineno)d) %(message)s'
 
 DEFAULT_LEVEL_COLORS = {
     TRACE: colors['c_fg_cyan'] + colors['c_bright'],
@@ -41,7 +53,7 @@ class ColorizedFormatter(Formatter):
     ):
         self.level_colors = level_colors or DEFAULT_LEVEL_COLORS
         fmt = fmt or DEFAULT_FORMAT
-        self._formatter = ExceptionFormatter(colorize=True, backtrace=False, diagnose=True,
+        self._formatter = ExceptionFormatter(colorize=not NO_COLOR, backtrace=False, diagnose=True,
                                              hide_lib_diagnose=hide_lib_diagnose)
 
         kwargs = {
@@ -60,6 +72,10 @@ class ColorizedFormatter(Formatter):
         super().__init__(**kwargs)
 
     def format(self, record: LogRecord) -> str:
+        # support for https://no-color.org/
+        if NO_COLOR:
+            return super().format(record)
+
         color = self.level_colors.get(record.levelno, self.level_colors.get(None, ""))
         orig = record.__dict__
         record.__dict__ = {**colors, "c_color": color, **record.__dict__}
